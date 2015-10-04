e16257772e096352d7be8f39e55232c907ae3a23
takatori <takatori@ws.cs.kobe-u.ac.jp>
2015-10-05 03:38:30 +0900

    Add create function to image model

--- app/models/Image.scala
+++ app/models/Image.scala
@@ -10,6 +10,16 @@ case class Image(id: Long, image_url: String, user_id: String)
 
 object Image {
 
+  def create(image_url: String, user_id: Option[String]) {
+
+    DB.withConnection {implicit c =>
+      SQL("insert into image(image_url, user_id) values ({image_url}, {user_id})").on(
+        'image_url -> image_url,
+        'user_id -> user_id
+      ).executeUpdate()
+    }
+  }
+
   val image = {
     get[Long]("id") ~
     get[String]("image_url") ~
