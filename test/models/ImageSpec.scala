a84525f711fc542feead01093df2f32b058bef8e
takatori <takatori@ws.cs.kobe-u.ac.jp>
2015-10-13 00:48:05 +0900

    Add model test

---
+++ test/models/ImageSpec.scala
@@ -0,0 +1,28 @@
+import org.specs2.mutable._
+import org.specs2.runner._
+import org.junit.runner._
+
+import play.api.test._
+import play.api.test.Helpers._
+
+import models.Image
+
+
+@RunWith(classOf[JUnitRunner])
+class ImageSpec extends Specification {
+
+  "Image#create" should {
+    "return id: Option[Long] if succeed in saveing to DB" in new WithApplication {
+      val image_url = "./tmp/test.png"
+      val user_id: Option[String] = None      
+      val id = Image.create(image_url, user_id)
+      id must beSome // check if an element is Some(_)
+    }
+
+    "throw Exception if image_url is Null" in new WithApplication {
+      val image_url = null
+      val user_id: Option[String] = Some("takatori")      
+      Image.create(image_url, user_id) must throwA[Exception]
+    }
+  }
+}
