dc4dcca46de319326d122dc4e2e3e119f359a7aa
takatori <takatori@ws.cs.kobe-u.ac.jp>
2015-10-29 22:17:50 +0900

    Remove not null constraint from user name

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -4,7 +4,7 @@
 
 CREATE TABLE user (
     user_id       varchar(255) NOT NULL,
-    name          varchar(255) NOT NULL,
+    name          varchar(255),
     avatar_url    varchar(512),
     PRIMARY KEY (user_id)
 );
