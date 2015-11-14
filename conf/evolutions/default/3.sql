45217be5bebac1582093738fefb414f38fafc597
takatori <takatori@ws.cs.kobe-u.ac.jp>
2015-11-14 15:09:39 +0900

    Add foreign key constraint to favorite table

--- conf/evolutions/default/3.sql
+++ conf/evolutions/default/3.sql
@@ -5,7 +5,9 @@
 CREATE TABLE favorite (
        user_id        varchar(255) NOT NULL,
        id             integer NOT NULL,
-       PRIMARY KEY (user_id, id)
+       PRIMARY KEY (user_id, id),
+       FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE CASCADE,
+       FOREIGN KEY (id) REFERENCES image(id) ON DELETE CASCADE
 );
 
 
